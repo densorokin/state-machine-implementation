9dc022437bbaa8daf21b4a7d8290ded7b3f10f9a
Denis Sorokin <densorokin@outlook.com>
2024-02-17 14:25:22 +0200

    chore: addded config

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { a } from "./app";
+
+console.log("index.js >>>");
+
+console.log("index.ts >>>", a(1));
---
+++ webpack.config.ts
@@ -0,0 +1,26 @@
+import path from "path";
+import { Configuration } from "webpack";
+import { buildWebpackConfig } from "./config/build/buildWebpackConfig";
+import { BuildEnv, BuildPath } from "./config/build/types/config";
+import { DEFAULT_MODE, DEFAULT_PORT } from "./config/build/consts";
+
+export default (env: BuildEnv) => {
+  const paths: BuildPath = {
+    entry: path.resolve(__dirname, "src", "index.ts"),
+    build: path.resolve(__dirname, "build"),
+    html: path.resolve(__dirname, "public", "index.html"),
+  };
+
+  const mode = env.mode || DEFAULT_MODE;
+  const isDev = mode === DEFAULT_MODE;
+  const port = env.port || DEFAULT_PORT;
+
+  const config: Configuration = buildWebpackConfig({
+    mode,
+    paths,
+    isDev,
+    port,
+  });
+
+  return config;
+};
