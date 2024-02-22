b085eea013d36a20f94e25427e88c3efd0e2b4e4
Denis Sorokin <densorokin@outlook.com>
2024-02-22 16:38:23 +0200

    refactor: styles

--- src/pages/StateMachinePage/ui/StateMachinePage.tsx
+++ src/pages/StateMachinePage/ui/StateMachinePage.tsx
@@ -1,11 +1,12 @@
 import { Questionnaire } from "features/Questionnaire/Questionnaire";
+import cls from "./StateMachinePage.module.scss";
 interface StateMachinePageProps {
   className?: string;
 }
 
 const StateMachinePage = ({ className }: StateMachinePageProps) => {
   return (
-    <div>
+    <div className={cls["state-machine-page"]}>
       <Questionnaire />
     </div>
   );
