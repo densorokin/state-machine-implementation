d1ee1ca16aaf75c2c475a88a81928a4bb53614ec
Denis Sorokin <densorokin@outlook.com>
2024-02-22 16:13:06 +0200

    chore: temp

--- src/widgets/Card/Card.tsx
+++ src/widgets/Card/Card.tsx
@@ -1,4 +1,3 @@
-import { CSSTransition } from "react-transition-group";
 import { Button } from "shared/ui/Button/Button";
 import cls from "./Card.module.scss";
 import { ReactNode } from "react";
@@ -28,37 +27,37 @@ export const Card = ({
   subtitle,
   title,
 }: CardProps) => {
+  if (!active) return;
+
   return (
-    <CSSTransition in={active} timeout={300} classNames="swipe" unmountOnExit>
-      <div data-testid={dataTestId}>
-        <div className={cls.card}>
-          <header className={cls.header}>
-            <h2 className={cls.title}>{title}</h2>
-            <p className={cls.subtitle}>{subtitle}</p>
-          </header>
-          <div className={cls.main}>{children}</div>
-          <div className={cls.actions}>
-            {prev && (
-              <Button
-                dataTestId={`${dataTestId}-prev-card-btn`}
-                className={cls.btn_prev}
-                onClick={prev.action}
-              >
-                {prev.content}
-              </Button>
-            )}
-            {next && (
-              <Button
-                className={cls.btn_next}
-                onClick={next.action}
-                dataTestId={`${dataTestId}-next-card-btn`}
-              >
-                {next.content}
-              </Button>
-            )}
-          </div>
+    <div data-testid={dataTestId}>
+      <div className={cls.card}>
+        <header className={cls.header}>
+          <h2 className={cls.title}>{title}</h2>
+          <p className={cls.subtitle}>{subtitle}</p>
+        </header>
+        <div className={cls.main}>{children}</div>
+        <div className={cls.actions}>
+          {prev && (
+            <Button
+              dataTestId={`${dataTestId}-prev-card-btn`}
+              className={cls.btn_prev}
+              onClick={prev.action}
+            >
+              {prev.content}
+            </Button>
+          )}
+          {next && (
+            <Button
+              className={cls.btn_next}
+              onClick={next.action}
+              dataTestId={`${dataTestId}-next-card-btn`}
+            >
+              {next.content}
+            </Button>
+          )}
         </div>
       </div>
-    </CSSTransition>
+    </div>
   );
 };
